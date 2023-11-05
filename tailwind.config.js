545c39293934d5a8f3d998da3226914b13a33803
XiovV <xiovv@tutanota.com>
2023-11-05 12:38:18 +0100

    feat: add tooltip above the email icon

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./src/*.{html,js}"],
   theme: {
     extend: {},
     fontFamily: {
       heebo: "Heebo, sans-serif",
     },
   },
-  plugins: [],
+  plugins: [require("daisyui")],
 };
